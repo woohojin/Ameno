a6000f7aaac1eb1a1c562a8a35cd2e2729dfb2fc
krgdy <krgdy@pusan.ac.kr>
2023-04-29 16:31:04 +0900

    modified:   commands/default/test.js

--- commands/default/test.js
+++ commands/default/test.js
@@ -4,7 +4,8 @@ module.exports = {
   data: new SlashCommandBuilder().setName("test").setDescription("test about web"),
   async execute(interaction) {
     Client.on("MessageCreate", (message) => {
-        message.create
+        console.log(message.content);
+        interaction.reply(message.content);
     },{once:true})
     // interaction.guild is the object representing the Guild in which the command was run
     await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
