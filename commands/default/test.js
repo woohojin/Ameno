557fd6541940373aa84691f21648f30ba5014304
Woohojin <hojin0624@gmail.com>
2023-04-29 16:41:31 +0900

    deploy-error delete GUILD_ID Constant in rest

--- commands/default/test.js
+++ commands/default/test.js
@@ -3,10 +3,14 @@ const { SlashCommandBuilder, Client } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder().setName("test").setDescription("test about web"),
   async execute(interaction) {
-    Client.on("MessageCreate", (message) => {
+    Client.on(
+      "MessageCreate",
+      (message) => {
         console.log(message.content);
         interaction.reply(message.content);
-    },{once:true})
+      },
+      { once: true }
+    );
     // interaction.guild is the object representing the Guild in which the command was run
     await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
   },
