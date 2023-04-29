7e116afb5a0ca20e343d1fba42479979209c6567
krgdy <krgdy@pusan.ac.kr>
2023-04-29 17:05:07 +0900

    for the test

--- commands/default/test.js
+++ commands/default/test.js
@@ -1,12 +1,14 @@
-const { SlashCommandBuilder, Client } = require("discord.js");
-
+const { SlashCommandBuilder, Client, Events, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers] });
 module.exports = {
   data: new SlashCommandBuilder().setName("test").setDescription("test about web"),
   async execute(interaction) {
-    Client.on("MessageCreate", (message) => {
+    console.log("test");
+    client.on("MessageCreate", (message) => {
+        console.log("event test");
         console.log(message.content);
         interaction.reply(message.content);
-    },{once:true})
+    })
     // interaction.guild is the object representing the Guild in which the command was run
     await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
   },
--- deploy-commands.js
+++ deploy-commands.js
@@ -32,7 +32,7 @@ const rest = new REST().setToken(TOKEN);
     console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(Routes.applicationGuildCommands(CLIENT_ID), { body: commands });
+    const data = await rest.put(Routes.applicationGuildCommands(CLIENT_ID,GUILD_ID), { body: commands });
 
     console.log(`Successfully reloaded ${data.length} application (/) commands.`);
   } catch (error) {
