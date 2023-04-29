4f5fda279a5dfd9b461e301d5c10c115a7c81bef
krgdy <krgdy@pusan.ac.kr>
2023-04-29 16:06:07 +0900

    new file:   commands/default/test.js

---
+++ commands/default/test.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder, Client } = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder().setName("test").setDescription("test about web"),
+  async execute(interaction) {
+    Client.on("MessageCreate", (message) => {
+        message.create
+    },{once:true})
+    // interaction.guild is the object representing the Guild in which the command was run
+    await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+  },
+};
